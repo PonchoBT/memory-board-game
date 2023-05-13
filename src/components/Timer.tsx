2031eae61c285b32946892ce30b418eb15ecc640
Alfonso BT <ponchobt.dev@gmail.com>
2023-05-13 01:16:34 -0600

    e

---
+++ src/components/Timer.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+let intervalId: NodeJS.Timeout | null = null;
+
+interface TimerProps {
+  reiniciar: boolean;
+  onReiniciarJuego: () => void;
+}
+
+const Timer: React.FC<TimerProps> = ({ reiniciar, onReiniciarJuego }) => {
+  const [actualTime, setActualTime] = useState(0);
+  const [btnPlayPause, setBtnPlayPause] = useState("Play");
+  const [isPaused, setIsPaused] = useState(false);
+
+  const initTimer = () => {
+    if (intervalId || isPaused) {
+      setBtnPlayPause("Play");
+      pauseTimer();
+      setIsPaused(false);
+      window.location.reload()
+    } else {
+      intervalId = setInterval(() => {
+        setActualTime((prevTime) => prevTime + 1);
+      }, 1000);
+      setBtnPlayPause("Pause");
+    }
+  };
+
+  const pauseTimer = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+      setIsPaused(true);
+    }
+  };
+
+  const clearTimer = () => {
+    setActualTime(0);
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+    setBtnPlayPause("Play");
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (reiniciar) {
+      clearTimer();
+    }
+  }, [reiniciar]);
+
+  const formatTime = () => {
+    const getSeconds = `0${actualTime % 60}`.slice(-2);
+    const minutes = `${Math.floor(actualTime / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(actualTime / 3600)}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
+  return (
+    <div>
+      <div>
+        <Typography variant="h3">{formatTime()}</Typography>
+      </div>
+
+      <Button variant="contained" color="primary" onClick={initTimer}>
+        {btnPlayPause}
+      </Button>
+    </div>
+  );
+};
+
+export default Timer;
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,128 +1,140 @@
-import { useState } from 'react'
-import Cardss from './card'
-import { api } from '../services/cardService';
-import Swal from 'sweetalert2';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-
+import { useState } from "react";
+import Cardss from "./card";
+import { api } from "../services/cardService";
+import Swal from "sweetalert2";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Timer from "./Timer";
 
 function Cards() {
-    const [items, setItems] = useState(api.getCard);
-    const [ganados, setGanados] = useState(0);
-    const [fallidos, setFallidos] = useState(0);
-    const [prev, setPrev] = useState(-1)
+  const [items, setItems] = useState(api.getCard);
+  const [ganados, setGanados] = useState(0);
+  const [fallidos, setFallidos] = useState(0);
+  const [prev, setPrev] = useState(-1);
+  const [reiniciar, setReiniciar] = useState(false);
 
-    function check(current: number) {
-
-        if (fallidos == 5) {
-           
-        } else {
-
-            if (items[current].id == items[prev].id) {
-                const aciertos = ganados + 1;
-                setGanados(aciertos);
-                items[current].stat = "correct"
-                items[prev].stat = "correct"
-                setItems([...items])
-                setPrev(-1)
-
-
-                if (aciertos == 9) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Felicidades, ganaste!!. 🥳'
-                    })
-
-                }
-            } else {
-                items[current].stat = "wrong"
-                items[prev].stat = "wrong"
-                setItems([...items])
-                setTimeout(() => {
-                    items[current].stat = ""
-                    items[prev].stat = ""
-                    setItems([...items])
-                    setPrev(-1)
-                }, 1000)
-                const fallos = fallidos + 1;
-                setFallidos(fallos);
-                if (fallos == 5) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Perdiste 😔',
-                        text: 'Fin del juego'
-                    })
-                }
-            }
+  function check(current: number) {
+    if (fallidos == 5) {
+    } else {
+      if (items[current].id == items[prev].id) {
+        const aciertos = ganados + 1;
+        setGanados(aciertos);
+        items[current].stat = "correct";
+        items[prev].stat = "correct";
+        setItems([...items]);
+        setPrev(-1);
+        if (aciertos == 1) {
+          Swal.fire({
+            icon: "success",
+            title: "Felicidades, ganaste!!. 🥳",
+          });
         }
-    }
-
-    function handleClick(id: number) {
-
-        if (prev === -1) {
-            items[id].stat = "active"
-            setItems([...items])
-            setPrev(id)
-        } else {
-            check(id)
+      
+      } else {
+        items[current].stat = "wrong";
+        items[prev].stat = "wrong";
+        setItems([...items]);
+        setTimeout(() => {
+          items[current].stat = "";
+          items[prev].stat = "";
+          setItems([...items]);
+          setPrev(-1);
+        }, 1000);
+        const fallos = fallidos + 1;
+        setFallidos(fallos);
+        if (fallos == 5) {
+    
+          Swal.fire({
+            icon: "error",
+            title: "Perdiste 😔",
+            text: "Fin del juego",
+          });
         }
+      }
     }
-
-    function reiniciarJuego() {
-        const reiniciarCards = api.getCard.sort(() => Math.random() - 0.5)
-        for (let index = 0; index < reiniciarCards.length; index++) {
-            const card = reiniciarCards[index];
-            card.stat = "";
-        }
-        setItems(reiniciarCards);
-        setFallidos(0);
-        setGanados(0);
+  }
+  function handleClick(id: number) {
+    if (prev === -1) {
+      items[id].stat = "active";
+      setItems([...items]);
+      setPrev(id);
+    } else {
+      check(id);
     }
+  }
 
+  function reiniciarJuego() {
+    const reiniciarCards = api.getCard.sort(() => Math.random() - 0.5);
+    for (let index = 0; index < reiniciarCards.length; index++) {
+      const card = reiniciarCards[index];
+      card.stat = "";
+    }
+    setItems(reiniciarCards);
+    setFallidos(0);
+    setGanados(0);
+    setReiniciar(!reiniciar);
 
-    return (
-        <Container maxWidth="xl">
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid item xs={12}>
-                    <Typography variant="h3" gutterBottom align='center' sx={{ mb: 5, mt:5 }}>
-                    Juego Memorama
-                    </Typography>
-                </Grid>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={8} lg={8}>
-                        <Card className='card1'>
-                            <CardContent>
-                                <Grid container xs={12} lg={12}>
-                                    {items.map((item, index) => (
-                                        <Grid item xs={4} md={2}>
-                                            <Cardss key={index} item={item} id={index} handleClick={handleClick} />
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </CardContent>
-                        </Card>
+    window.location.reload();
+  }
+
+  return (
+    <Container maxWidth="xl">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid item xs={12}>
+          <Typography
+            variant="h3"
+            gutterBottom
+            align="center"
+            sx={{ mb: 5, mt: 5 }}
+          >
+            Juego Memorama
+          </Typography>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={8} lg={8}>
+            <Card className="card1">
+              <CardContent>
+                <Grid container>
+                  {items.map((item, index) => (
+                    <Grid item xs={4} md={2} key={index}>
+                      <Cardss
+                        key={index}
+                        item={item}
+                        id={index}
+                        handleClick={handleClick}
+                      />
                     </Grid>
-                    <Grid item xs={12} sm={4} lg={4}>
-                        <Card className='card2'>
-                            <CardContent>
-                                <Typography variant="h6" color={"red"} gutterBottom>
-                                    Fallidos: {fallidos}
-                                </Typography>
-                                <Grid xs={12} lg={6} margin={"auto"} mt={4}>
-                                <Button variant="contained" color="success" onClick={reiniciarJuego} sx={{ width: '100%'}}>Reiniciar Juego</Button>
-                                </Grid>
-                            </CardContent>
-                        </Card>
-                    </Grid>
+                  ))}
                 </Grid>
-            </Box>
-        </Container>
-    )
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={4} lg={4}>
+            <Card className="card2">
+              <CardContent>
+                <Typography variant="h6" color={"red"} gutterBottom>
+                  Fallidos: {fallidos}
+                </Typography>
+                <Grid item xs={12} lg={6} margin={"auto"} mt={4}>
+                  <Timer
+                    reiniciar={reiniciar}
+                    onReiniciarJuego={reiniciarJuego}
+                  />
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
 }
 
-export default Cards
+export default Cards;
+function setBtnPlayPause(arg0: string) {
+  throw new Error("Function not implemented.");
+}
